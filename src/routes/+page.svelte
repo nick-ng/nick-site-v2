c3083fc911866e56565b086fbe6e38794785b669
Nick Ng <nick-ng@users.noreply.github.com>
2024-02-17 18:32:23 +1300

    fix: add API_HOST environment variable to github actions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script>
-	import { PUBLIC_API_HOST } from '$env/static/public';
+	import { API_HOST } from '$env/static/public';
 </script>
 
 <div>
-	<h1>Meal Tracker</h1>
-	<p>API Host: {PUBLIC_API_HOST}</p>
+	<h1>V2 Site!</h1>
+	<p>API Host: {API_HOST}</p>
 	<button
 		on:click={async () => {
-			const res = await fetch(`${PUBLIC_API_HOST}/hello`);
+			const res = await fetch(`${API_HOST}/hello`);
 			const resText = await res.text();
 
 			console.log('resText', resText);
